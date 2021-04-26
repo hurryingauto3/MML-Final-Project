5a1ed3a8e3c628dba548d8164771146f05feba2e
Affan Amir Mir <54717435+Affanmir@users.noreply.github.com>
2021-04-26 12:10:02 +0500

    Update temp_Octree.py

--- temp_Octree.py
+++ temp_Octree.py
@@ -228,7 +228,27 @@ def Vertex_Reduction(Lst,Alpha,N):
             if pow(Lst[i][0]-Lst[i-1][0] , 2) + pow(Lst[i][1]-Lst[i-1][1] , 2) + pow(Lst[i][2]-Lst[i-1][2] , 2) <= Alpha:
                 Lst.pop(i)
     return Lst
+    
         
+def point_cloud(ob_name, coords, faces=[], edges=[]):
+    """Create point cloud object based on given coordinates and name.
+
+    Keyword arguments:
+    ob_name -- new object name
+    coords -- float triplets eg: [(-1.0, 1.0, 0.0), (-1.0, -1.0, 0.0)]
+    """
+
+    # Create new mesh and a new object
+    me = bpy.data.meshes.new(ob_name + "Mesh")
+    ob = bpy.data.objects.new(ob_name, me)
+
+    # Make a mesh from a list of vertices/edges/faces
+    me.from_pydata(coords, edges, faces)
+
+    # Display name and update the mesh
+    ob.show_name = True
+    me.update()
+    return ob
 
 
 
